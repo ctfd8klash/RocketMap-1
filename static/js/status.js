1235a64f8ae6ebe5c2795dfd957dd4bc4f19fd80
tomballgithub <jeoff@krontz.com>
2017-04-02 09:53:49 -0500

    Removed duplicate statmsg line

--- static/js/status.js
+++ static/js/status.js
@@ -273,7 +273,6 @@ function addTotalStats(result) {
         }
 
         statmsg = 'Total active: ' + active + ' | Success: ' + success.toFixed() + ' (' + sph.toFixed(1) + '/hr) | Fails: ' + failed.toFixed() + ' (' + fph.toFixed(1) + '/hr) | Empties: ' + empty.toFixed() + ' (' + eph.toFixed(1) + '/hr) | Skips: ' + skipped.toFixed() + ' (' + skph.toFixed(1) + '/hr) | Captchas: ' + captcha.toFixed() + ' (' + cph.toFixed(1) + '/hr) ($' + ccost.toFixed(5) + '/hr, $' + cmonth.toFixed(3) + '/mo) | Elapsed:  ' + elapsedHours.toFixed(1) + 'h (' + elapsedSecs.toFixed(0) + 's)<hr />'
-        statmsg = 'Total active: ' + active + ' | Success: ' + success.toFixed() + ' (' + sph.toFixed(1) + '/hr) | Fails: ' + failed.toFixed() + ' (' + fph.toFixed(1) + '/hr) | Empties: ' + empty.toFixed() + ' (' + eph.toFixed(1) + '/hr) | Skips: ' + skipped.toFixed() + ' (' + skph.toFixed(1) + '/hr) | Captchas: ' + captcha.toFixed() + ' (' + cph.toFixed(1) + '/hr) ($' + ccost.toFixed(5) + '/hr, $' + cmonth.toFixed(3) + '/mo) | Elapsed:  ' + elapsedHours.toFixed(1) + 'h (' + elapsedSecs.toFixed(0) + 's)<hr />'
         if (mainWorkers > 1) {
             title = '(Total Statistics across ' + mainWorkers + ' instances)'
         } else {
